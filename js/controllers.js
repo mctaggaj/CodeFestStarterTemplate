53c2a2c618bff82692b41eaaf17ddc6cf98d4901
Richard Hepburn <richardh@cetaris.com>
2014-06-06 15:33:34 -0400

    Updated e2e Tests and revised comments.

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,37 @@
 'use strict';
 
-/* Controllers */
+/* Define Controllers Here */
 
-angular.module('myApp.controllers', [])
+var appModule = angular.module('myApp.controllers', []);
 
-.controller('TestController', function ($scope) {
+/* Controller for the application */
+appModule.controller('AppController', function ($scope, $route) {
+
+
+});
+
+/* Controller for the Home page */
+appModule.controller('HomeController', function ($scope) {
+    $scope.WelcomeMessage = "Welcome!"
+});
+
+/* Controller for the Bootstrap Examples page */
+appModule.controller('BootstrapExamplesController', function ($scope) {
+});
+
+/* Controller for the Angular Examples page */
+appModule.controller('AngularExamplesController', function ($scope) {
+});
+
+
+/* Controller for the Getting Started page */
+appModule.controller('GettingStartedController', function ($scope) {
+});
+
+/* Controller for the Testing page */
+appModule.controller('TestController', function ($scope) {
     $scope.Result = "n/a";
     $scope.TestMe = function () {
         $scope.Result = "Success!";
     };
-})
-;
+});
